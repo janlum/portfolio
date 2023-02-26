d97859c71f5f0cb81388889f1598a52a80dad514
janlum <janlum8@gmail.com>
2023-02-26 16:26:42 +1100

    test mobile hamburger

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,3 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
+['click', 'ontouchstart'].forEach( evt => document.querySelector('.hamburger').addEventListener(evt, () => {
   document.querySelector('.nav-links').classList.toggle('expanded');
-});
+}));

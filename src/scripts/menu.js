f6a1d539cc5f1d59da3d703235aebe59e476bff7
janlum <janlum8@gmail.com>
2023-02-26 22:21:49 +1100

    test mobile

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,8 @@
 ['click', 'ontouchstart'].forEach( evt => document.querySelector('.hamburger').addEventListener(evt, () => {
+  console.log("event start")
+  // var touchobj = e.changedTouches[0]; 
+  // startx = parseInt(touchobj.clientX); 
+  // starty = parseInt(touchobj.clientY); 
   document.querySelector('.nav-links').classList.toggle('expanded');
+  console.log("event start done")
 }));

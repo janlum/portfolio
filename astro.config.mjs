42e6a6c1e36f47c6f037fcbce48f270a02239ce3
janlum <janlum8@gmail.com>
2023-02-27 17:27:35 +1100

    testing github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,5 @@ import preact from "@astrojs/preact";
 export default defineConfig({
   integrations: [preact()],
   site: 'https://janlum.github.io',
-  base: '/src',
+  base: '/portfolio/src',
 });
